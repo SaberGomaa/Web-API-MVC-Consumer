50b92cbfd2c140c7d473557cffca612cd684894d
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-30 22:42:29 +0200

    Get Data from API and display it

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Web_API_MVC_Consumer.Models;
 
 namespace Web_API_MVC_Consumer.Controllers
 {
@@ -17,8 +19,17 @@ namespace Web_API_MVC_Consumer.Controllers
 
         public ActionResult view()
         {
-            
-            return View();
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://127.0.0.1:82/api/employee").Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var emps = result.Content.ReadAsAsync<List<Employee>>().Result;
+                return View(emps);
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
         }
 
     }
