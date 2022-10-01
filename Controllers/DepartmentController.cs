f8243a6649e06e2ac5fc06cf9f3b4174ff416c30
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-01 21:01:52 +0200

    Departments OPR

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Web_API_MVC_Consumer.Models;
 
 namespace Web_API_MVC_Consumer.Controllers
 {
@@ -14,7 +16,23 @@ namespace Web_API_MVC_Consumer.Controllers
             return View();
         }
 
-        
+        public ActionResult view()
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://localhost:82/api/department").Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                var depts = result.Content.ReadAsAsync<List<Department>>().Result;
+                return View(depts);
+            }
+            else
+            {
+                return RedirectToAction("NotFound");
+            }
+
+
+        }
 
     }
 }
