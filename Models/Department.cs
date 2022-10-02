10f2b0b3c529b9efc6d35ef4c903923278e4ec52
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-02 13:02:01 +0200

    Dept Validation

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace Web_API_MVC_Consumer.Models
     public class Department
     {
         public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage = "*")]
         public string Name { get; set; }
     }
 }
