5c5801c191a77cca367f69a292925ba39f0bcc46
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-02 12:51:48 +0200

    Validation Jquery

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,14 @@ namespace Web_API_MVC_Consumer.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="*")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "*")]
+
         public int Age { get; set; }
+        [Required(ErrorMessage = "*")]
         public decimal Salary { get; set; }
+        [Required(ErrorMessage = "*")]
         public int DepartmentId { get; set; }
 
         public virtual Department Department { get; set; }
